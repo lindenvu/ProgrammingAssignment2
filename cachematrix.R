1d7bc00d8294ac82ca2b850fb38c48686d1cefdc
lindenvu <github@lindenvu.com>
2014-05-25 17:08:54 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  makeCacheMatrix creates a special "vector/list of functions"
+##  cacheSolve takes the vector from above function, it gets the solved matrix if its already been cached, otherwise it calculates the value of the solved matrix and caches the value
 
-## Write a short comment describing this function
+#The first function, makeCacheMatrix creates a special "vector", which is really a list containing a function to
+#set the value of the matrix
+#get the value of the matrix
+#set the value of the solved matrix
+#get the value of the solved matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+#The following function calculates the solved matrix of the special "vector" created with the above function. 
+#However, it first checks to see if the solved matrix has already been calculated. 
+#If so, it gets the solved matrix from the cache and skips the computation. 
+#Otherwise, it calculates the solved matrix of the data and sets the value of the solved matrix in the cache via the setsolve function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
-
